d61f2f340086b8e9cb57cc4076f3531f4bdb1f57
ChalitaK24 <chalurga@gmail.com>
2024-09-30 19:23:20 -0700

    done crr

--- 0x05-python-exceptions/4-list_division.py
+++ 0x05-python-exceptions/4-list_division.py
@@ -11,7 +11,7 @@ def list_division(my_list_1, my_list_2, list_length):
             division = 0
         except TypeError:
             print("wrong type")
-            division =  0
+            division = 0
         except ZeroDivisionError:
             print("division by 0")
             division = 0
