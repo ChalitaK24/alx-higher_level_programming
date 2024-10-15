7d7334a1f4903073c27b29d252cc4f778b190e48
ChalitaK24 <schalitakel@gmial.com>
2024-10-15 16:35:59 -0700

    crr

--- 0x0B-python-input_output/3-to_json_string.py
+++ 0x0B-python-input_output/3-to_json_string.py
@@ -2,8 +2,9 @@
 """Converts objects to json"""
 import json
 
-def to_json_string(my_obj):
+
+def to_json_string(my_str):
     """
     Returns the JSON representation of an object.
     """
-    return json.dumps(my_obj)
+    return json.dumps(my_str)
