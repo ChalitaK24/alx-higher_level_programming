c8dffcad64af607053cdeb9c5a7cd92bbe0c5352
ChalitaK24 <schalitakel@gmial.com>
2024-10-15 14:40:37 -0700

    crrr taks 0

--- 0x0B-python-input_output/0-read_file.py
+++ 0x0B-python-input_output/0-read_file.py
@@ -1,5 +1,16 @@
 #!/usr/bin/python3
+    """
+    Function that reads a text file.
+    Functions:
+        read_file: reads a text file and prints to stdout.
+    """
 
 def read_file(filename=""):
+    """
+    Reads a text file and prints to standard outpus.
+    Returns:
+        None
+    """
     with open(filename, 'r', encoding='utf-8') as file:
         print(file.read(), end="")
+
