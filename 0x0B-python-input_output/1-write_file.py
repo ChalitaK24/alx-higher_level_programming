837474223e60541d7ed387340b1e7ca777b214cc
ChalitaK24 <schalitakel@gmial.com>
2024-10-15 15:47:09 -0700

    taks 1

---
+++ 0x0B-python-input_output/1-write_file.py
@@ -0,0 +1,11 @@
+#!/usr/bin/bash
+def write_file(filename="", text=""):
+    """
+    Write str to a UTF-8 encoded txt file and returns numberof char
+
+    Returns:
+    int: the number of charcters written
+    """
+    with open(filename, 'w', encoding='utf-8') as file:
+        no_char = file.write(text)
+    return no_char
