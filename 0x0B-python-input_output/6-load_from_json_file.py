0581e8ad45d22f0bb40f44ba4f11ad8a563c7db9
ChalitaK24 <schalitakel@gmial.com>
2024-10-16 03:09:46 -0700

    task 6 crr

--- 0x0B-python-input_output/6-load_from_json_file.py
+++ 0x0B-python-input_output/6-load_from_json_file.py
@@ -1,14 +1,14 @@
 #!/usr/bin/python3
 """Creates an Object (Python data structure) from a JSON file """
 import json
-"""
-filename (str): The name of the file containing the JSON str.
-Returns:
-    Object: The Python object represented by the JSON str in the file.
-
-"""
 
 
 def load_from_json_file(filename):
+    """
+    filename (str): The name of the file containing the JSON str.
+    Returns:
+        Object: The Python object represented by the JSON str in the file.
+
+    """
     with open(filename, 'r', encoding='utf-8') as file:
         return json.load(file)
