60e0e6649254696c679b985108b1d0fc1d145f3e
ChalitaK24 <schalitakel@gmial.com>
2024-10-16 07:53:31 -0700

    task 11 crr

--- 0x0B-python-input_output/11-student.py
+++ 0x0B-python-input_output/11-student.py
@@ -1,14 +1,13 @@
 #!/usr/bin/python3
 """ Class that defines a student with first name, last name and age."""
 
+
 class Student:
     """
     first_name (str): The first name of the student.
     last_name (str): The last name of the student.
     age (int): The age of the student.
     """
-
-
     def __init__(self, first_name, last_name, age):
         """
         Function initializes Student with first name, last name, and age.
@@ -16,17 +15,19 @@ class Student:
         self.first_name = first_name
         self.last_name = last_name
         self.age = age
+
     def to_json(self, a=None):
         """
         Function to retrive dictionary representation of Student
         a (list): A list of attribute names to retrieve (optional).
          Returns:
-            dict: A dictionary containing the specified attributes, or all attributes.
+            dict: A dictionary containing the specified attr, or all attr.
         """
         if a is None:
             return self.__dict__
         elif isinstance(a, list) and all(isinstance(attr, str) for attr in a):
-            return {attr: getattr(self, attr) for attr in a if hasattr(self, attr)}
+            return {attr: getattr(self, attr) for attr in a
+                    if hasattr(self, attr)}
         else:
             return self.__dict__
 
