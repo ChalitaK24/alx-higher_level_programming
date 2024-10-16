9368543afdb83a69cce7c11ace56e4ecac76d060
ChalitaK24 <schalitakel@gmial.com>
2024-10-16 04:00:47 -0700

    task 7 crr

--- 0x0B-python-input_output/7-add_item.py
+++ 0x0B-python-input_output/7-add_item.py
@@ -1,4 +1,6 @@
 #!/usr/bin/python3
+"""Adds all command-line arguments to a list and saves them to a JSON file."""
+
 import sys
 
 from os.path import exists
@@ -7,6 +9,15 @@ save_to_json_file = __import__('5-save_to_json_file').save_to_json_file
 load_from_json_file = __import__('6-load_from_json_file').load_from_json_file
 
 filename = 'add_item.json'
+
+
+def add_items_to_json():
+    """
+    Function adds all command line arguments to a list on a JSON file
+    Returns:
+        None
+    """
+
 
 if exists(filename):
 
