8d4bcf686ea15b2f87ee4d6a133067730b6279e5
ChalitaK24 <schalitakel@gmial.com>
2024-10-16 13:15:31 -0700

    task 0

---
+++ 0x0A-python-inheritance/0-lookup.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+
+"""Function Returns a list of available attr and mtd of obj"""
+
+def lookup(obj):
+    """
+    obj: the object to inspect
+    Returns: list of attr and mtds available in obj
+    """
+
+    return dir(obj)
