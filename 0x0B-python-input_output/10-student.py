4136bdbb71e3c48f8f949aaa6baa92f5a001bc0b
ChalitaK24 <schalitakel@gmial.com>
2024-10-16 06:50:29 -0700

    tsk 10 crrr

--- 0x0B-python-input_output/10-student.py
+++ 0x0B-python-input_output/10-student.py
@@ -15,6 +15,7 @@ class Student:
         self.first_name = first_name
         self.last_name = last_name
         self.age = age
+
     def to_json(self, a=None):
         """
         function retrives a dictionary representation of the Student instance.
