02bb28c144810b0b7cf32c7843e3602bb3b1f38a
ChalitaK24 <schalitakel@gmial.com>
2024-10-16 08:17:38 -0700

    task 12

--- 0x0B-python-input_output/12-pascal_triangle.py
+++ 0x0B-python-input_output/12-pascal_triangle.py
@@ -1,2 +1,22 @@
 #!/usr/bin/python3
+"""
+Function listing pascal's triangle of n
+"""
 
+
+def pascal_triangle(n):
+    """
+    n(int): number of rows
+    Returns: list
+    """
+    if n <= 0:
+        return[]
+
+    triangle = [[1]]
+
+    for i in range(1, n):
+        row_prv = tri[-1]
+        row = [1] + [row_prv[j] + row_prv[j + 1]
+                       for j in range(len(row_prv) - 1)] + [1]
+        triangle.append(row)
+    return triangle
