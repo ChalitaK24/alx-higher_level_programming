74feb845e4613c107a717df71feb7f7db4cdfcab
ChalitaK24 <schalitakel@gmial.com>
2024-10-15 17:04:06 -0700

    crr 4

--- 0x0B-python-input_output/4-from_json_string.py
+++ 0x0B-python-input_output/4-from_json_string.py
@@ -9,5 +9,10 @@ Returns an object rep by a JSON string
 
 
 def from_json_string(my_str):
-
+    """
+    Args:
+        my_str (str): The JSON string to be deserialized.
+    Returns:
+        object: The Python object represented by the JSON string.
+    """
     return json.loads(my_str)
