d9e95f9a87126d7b21453f598f422ac21657c1b8
ChalitaK24 <schalitakel@gmial.com>
2024-10-16 02:44:54 -0700

    task 5 crr

--- 0x0B-python-input_output/5-save_to_json_file.py
+++ 0x0B-python-input_output/5-save_to_json_file.py
@@ -2,12 +2,13 @@
 """Function that writes an object to a text file using a JSON rep"""
 
 import json
-"""
-my_obj (object): The python object to be serialized
-filename (str): The name of the file where the JSON str is written
-"""
 
 
 def save_to_json_file(my_obj, filename):
+    """
+    Returns an Object (Python Data structure) represented by JSON str
+        my_obj (object): The python object to be serialized
+        filename (str): The name of the file where the JSON str is written
+    """
     with open(filename, 'w', encoding='utf-8') as file:
         json.dump(my_obj, file)
