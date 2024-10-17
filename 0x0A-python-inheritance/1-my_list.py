108a5711e664f7b1f1d652163c2540932c7690e6
ChalitaK24 <schalitakel@gmial.com>
2024-10-16 18:15:48 -0700

    task 1 crr

--- 0x0A-python-inheritance/1-my_list.py
+++ 0x0A-python-inheritance/1-my_list.py
@@ -2,10 +2,15 @@
 
 """A class that inherits from the list"""
 
-class MyList(list):
 
-    def print_sorted(self):
-        """
-        Function prints the sorted list in ascending order
-        """
-        print(sorted(self))
+class MyList(list):
+    """
+    Subclass of the built-in list class to be printed in ascending order
+    """
+
+
+def print_sorted(self):
+    """
+    Function prints the sorted list in ascending order
+    """
+    print(sorted(self))
