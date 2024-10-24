45fd11b55833c1a82c750e4408cffb27408e912c
ChalitaK24 <schalitakel@gmail.com>
2024-10-24 13:59:15 -0700

    task 4crr

--- 0x02-python-import_modules/4-hidden_discovery.py
+++ 0x02-python-import_modules/4-hidden_discovery.py
@@ -2,9 +2,8 @@
 import hidden_4
 
 if __name__ == "__main__":
-    names = dir(hidden_4)
+    filtered_names = filter(lambda name:
+                            not name.startswith("__"), dir(hidden_4))
 
-
-    for name in sorted(names):
-        if not name.startswith("__"):
-            print(name)
+    for name in sorted(filtered_names):
+        print(name)
