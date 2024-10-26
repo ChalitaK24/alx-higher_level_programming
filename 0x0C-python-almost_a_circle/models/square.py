c0135df0cdf61165f05f5dc9ea387b7c33dbd456
ChalitaK24 <schalitakel@gmail.com>
2024-10-25 19:58:49 -0700

    task 11

--- 0x0C-python-almost_a_circle/models/square.py
+++ 0x0C-python-almost_a_circle/models/square.py
@@ -10,19 +10,11 @@ from models.rectangle import Rectangle
 class Square(Rectangle):
 
     """ class constructor"""
-
-    def __init__(self, size, x=0, y=0, id=None):
+     def __init__(self, size, x=0, y=0, id=None):
         """
         new Square instance.
         """
         super().__init__(size, size, x, y, id)
-
-    def __str__(self):
-        """
-        returns str representation
-        of tha Square
-        """
-        return f"[Square] ({self.id}) {self.x}/{self.y} - {self.width}"
 
     @property
     def size(self):
@@ -34,7 +26,7 @@ class Square(Rectangle):
     @size.setter
     def size(self, value):
         """
-        Get size of Square
+        set size of Square
         """
         self.width = value
         self.height = value
