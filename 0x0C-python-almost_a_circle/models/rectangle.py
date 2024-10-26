5dc126b2f9650c98b7162a03a3a992dbad08effa
ChalitaK24 <schalitakel@gmail.com>
2024-10-25 17:32:15 -0700

    crr task

--- 0x0C-python-almost_a_circle/models/rectangle.py
+++ 0x0C-python-almost_a_circle/models/rectangle.py
@@ -37,7 +37,7 @@ class Rectangle(Base):
 
         if not isinstance(value, int):
             raise TypeError("width must be an integer")
-        if value <= 0:
+        if value < 0:
             raise ValueError("width must be positive")
         self.__width = value
 
@@ -56,7 +56,7 @@ class Rectangle(Base):
 
         if not isinstance(value, int):
             raise TypeError("height must be an integer")
-        if value <= 0:
+        if value < 0:
             raise ValueError("height must be Posisitve")
         self.__height = value
 
