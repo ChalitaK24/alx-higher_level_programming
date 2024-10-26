a0b1778724196fe461a3c5886ac0fb18b6db2eed
ChalitaK24 <schalitakel@gmail.com>
2024-10-25 19:59:49 -0700

    task 11cc

--- 0x0C-python-almost_a_circle/models/square.py
+++ 0x0C-python-almost_a_circle/models/square.py
@@ -8,9 +8,9 @@ from models.rectangle import Rectangle
 
 
 class Square(Rectangle):
-
     """ class constructor"""
-     def __init__(self, size, x=0, y=0, id=None):
+
+    def __init__(self, size, x=0, y=0, id=None):
         """
         new Square instance.
         """
