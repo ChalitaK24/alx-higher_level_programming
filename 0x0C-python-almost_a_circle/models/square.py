c973fa4c00a4ab78145977929a26fc20cdcbbf2d
ChalitaK24 <schalitakel@gmail.com>
2024-10-25 19:45:55 -0700

    task crr

--- 0x0C-python-almost_a_circle/models/square.py
+++ 0x0C-python-almost_a_circle/models/square.py
@@ -4,8 +4,11 @@
 
 from models.rectangle import Rectangle
 
+""" class Square inherits from rectangle"""
 
 class Square(Rectangle):
+
+    """ class constructor"""
 
     def __init__(self, size, x=0, y=0, id=None):
         """
