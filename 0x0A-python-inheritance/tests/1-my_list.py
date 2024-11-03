8c2d60b7a6d245e53c468e7e02803dc5a8043cc5
ChalitaK24 <schalitakel@gmail.com>
2024-11-03 07:09:33 -0800

    test

---
+++ 0x0A-python-inheritance/tests/1-my_list.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+
+"""A class that inherits from the list"""
+
+
+class MyList(list):
+    """
+    Subclass of the built-in list class to be printed in ascending order
+    """
+    def print_sorted(self):
+        """
+        Function prints the sorted list in ascending order
+        """
+        print(sorted(self))
