2ebc65e26aa9ea8aa7c5fbb4e3069b4a783f51a2
ChalitaK24 <schalitakel@gmail.com>
2024-11-03 11:38:36 -0800

    task 8cr

--- 0x0A-python-inheritance/8-rectangle.py
+++ 0x0A-python-inheritance/8-rectangle.py
@@ -20,7 +20,7 @@ class BaseGeometry:
         """
         if not isinstance(value, int):
             raise TypeError(f"{name} must be in an integer")
-        if value < 0:
+        if value <= 0:
             raise ValueError(f"{name} must be greater than 0")
 
 class Rectangle(BaseGeometry):
