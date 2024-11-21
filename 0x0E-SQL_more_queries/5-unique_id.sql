27544a9bef8459f7aba51275d0ac03340c6eb2ab
ChalitaK24 <schalitakel@gmail.com>
2024-11-20 18:49:11 -0800

    sqql 5

--- 0x0E-SQL_more_queries/5-unique_id.sql
+++ 0x0E-SQL_more_queries/5-unique_id.sql
@@ -1,2 +1,2 @@
 -- creates the table unique_id with the specified columns in MySQL database
-CREAT TABLE OF NOT EXISTS unique_id (id INT DEAFULT 1 UNIQUE, name VARCHAR(256);
+CREATE TABLE IF NOT EXISTS unique_id (id INT DEFAULT 1 UNIQUE, name VARCHAR(256));
