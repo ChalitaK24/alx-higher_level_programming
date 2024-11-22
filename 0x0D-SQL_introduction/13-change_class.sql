5b6e2d24d53f65149f58d3095c53de8e95a33943
ChalitaK24 <schalitakel@gmail.com>
2024-11-21 17:48:26 -0800

    sql14 13cr

--- 0x0D-SQL_introduction/13-change_class.sql
+++ 0x0D-SQL_introduction/13-change_class.sql
@@ -1,3 +1,4 @@
 -- Removes all records with a score <= 5 in the table second_table
 -- of the database hbtn_0c_0 in MySQL server
-USE hbtn_0c_0; DELETE FROM second_table WHERE score <= 5;
+USE hbtn_0c_0;
+DELETE FROM second_table WHERE score <= 5;
