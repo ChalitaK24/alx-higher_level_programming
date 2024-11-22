6c600960c67df53da28924ca1a3538c67f1fdd08
ChalitaK24 <schalitakel@gmail.com>
2024-11-21 17:57:10 -0800

    sql

--- 0x0D-SQL_introduction/14-average.sql
+++ 0x0D-SQL_introduction/14-average.sql
@@ -1,4 +1,3 @@
 -- computes the score average of all records
 -- in the table second_table of the database hbtn_0c_0 in MySQL server
-USE hbtn_0c_0;
-SELECT COUNT(*) AS record_count, AVG(score) AS average from second_table;
+SELECT AVG(score) AS average FROM second_table;
