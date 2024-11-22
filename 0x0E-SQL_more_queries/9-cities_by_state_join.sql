60137d4d1d301dbebeb7d25275805981f2a65051
ChalitaK24 <schalitakel@gmail.com>
2024-11-22 05:04:40 -0800

    sqqql9

--- 0x0E-SQL_more_queries/9-cities_by_state_join.sql
+++ 0x0E-SQL_more_queries/9-cities_by_state_join.sql
@@ -1,13 +1,5 @@
--- lists all cities contained in the database hbtn_0d_usa
-SELECT
-	cities.id,
-	cities.name,
-	states.name AS state_name
-FROM
-	cities
-LEFT JOIN
-	states
-ON
-	cities.state_id = states.id
-ORDER BY
-	cities.id ASC;
+-- lists all cities contained in the database hbtn_0d_u
+SELECT cities.id, cities.name, states.name
+FROM cities
+JOIN states ON cities.states_id=states.id
+ORDER BY cities.id;
