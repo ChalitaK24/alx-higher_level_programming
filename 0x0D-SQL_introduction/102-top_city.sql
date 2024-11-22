68ddd71ce75376b5798b9c952ec7d5e6883d64b7
ChalitaK24 <schalitakel@gmail.com>
2024-11-21 18:17:15 -0800

    adv

--- 0x0D-SQL_introduction/102-top_city.sql
+++ 0x0D-SQL_introduction/102-top_city.sql
@@ -1,3 +1,3 @@
 -- display the top 3 cities with the highest temperatures during July and August,
 -- ordered by temperature in descending order
-SELECT city, MAX(value) AS max_temperature FROM temperatures WHERE month IN (7, 8) GROUP BY cityORDER BY max_temperature DESC LIMIT 3;
+SELECT city, MAX(value) AS max_temperature FROM temperatures WHERE month IN (7, 8) GROUP BY city ORDER BY max_temperature DESC LIMIT 3;
