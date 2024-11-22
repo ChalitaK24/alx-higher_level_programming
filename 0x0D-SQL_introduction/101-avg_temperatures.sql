8e60b7958eb5022b69947e0154749939a1f3f12e
ChalitaK24 <schalitakel@gmail.com>
2024-11-22 00:40:34 -0800

    18sql

--- 0x0D-SQL_introduction/101-avg_temperatures.sql
+++ 0x0D-SQL_introduction/101-avg_temperatures.sql
@@ -1,6 +1,6 @@
 -- displays the average temperature (Fahrenheit) 
 -- by city ordered by temperature (descending)
-SELECT city, AVG(value) AS average_temperature 
-FROM temperatures 
-GROUP BY city 
-ORDER BY average_temperature DESC;
+SELECT city, AVG(temperatre) AS average_temp
+FROM temperatures
+GROUP BY city
+ORDER BY average_temp DESC;
