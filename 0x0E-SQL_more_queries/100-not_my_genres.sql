94320f0e1daacf6cf436a90858c0a4c10efb25bc
ChalitaK24 <schalitakel@gmail.com>
2024-11-22 08:01:42 -0800

    sqql16cr

--- 0x0E-SQL_more_queries/100-not_my_genres.sql
+++ 0x0E-SQL_more_queries/100-not_my_genres.sql
@@ -1,1 +1,5 @@
 -- Uses the hbtn_0d_tvshows database to list all genres not linked to the show Dexter
+SELECT  tv_genres.name
+FROM tv_genres 
+WHERE  tv_genres.id NOT IN (SELECT  tv_show_genres.genre_id)
+ORDER BY tv_genres.name ASC;
