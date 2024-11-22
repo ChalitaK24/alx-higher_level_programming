48c9c07f6d2520584729b7fd0448d6532ac4fe29
ChalitaK24 <schalitakel@gmail.com>
2024-11-22 01:48:05 -0800

    crr sqql9

--- 0x0E-SQL_more_queries/9-cities_by_state_join.sql
+++ 0x0E-SQL_more_queries/9-cities_by_state_join.sql
@@ -1,2 +1,5 @@
 -- lists all cities contained in the database hbtn_0d_usa
-SELECT cities.id, cities.name, (SELECT states.name FROM states WHERE states.id = cities.state_id) AS state_name FROM cities ORDER BY cities.id ASC;
+SELECT cities.id, cities.name, states.name AS state_name 
+FROM cities, states
+WHERE cities.state_id = state.id
+ORDER BY cities.id ASC;
