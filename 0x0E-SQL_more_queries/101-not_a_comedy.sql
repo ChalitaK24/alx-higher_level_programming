d7d45c732e19e0f286e80acd44099e772f57b5b1
ChalitaK24 <schalitakel@gmail.com>
2024-11-24 04:59:09 -0800

    sqql18cr

--- 0x0E-SQL_more_queries/101-not_a_comedy.sql
+++ 0x0E-SQL_more_queries/101-not_a_comedy.sql
@@ -1,10 +1,13 @@
 -- lists all shows without the genre Comedy in the database hbtn_0d_tvshows.
 SELECT tv_shows.title
 FROM tv_shows
+LEFT JOIN tv_show_genres ON tv_shows.id = tv_show_genres.show_id
+LEFT JOIN tv_genres ON tv_show_genres.genre_id = tv_genres.id
 WHERE tv_shows.id NOT IN (
 	SELECT tv_show_genres.show_id 
 	FROM tv_show_genres 
-	JOIN tv_shows_genres ON tv_show_genres.genre_id = tv_genres.id 
+	INNER JOIN tv_genres ON tv_show_genres.genre_id = tv_genres.id 
 	WHERE tv_genres.name = 'Comedy'
 )
+GROUP BY tv_shows.id
 ORDER BY tv_shows.title ASC;
