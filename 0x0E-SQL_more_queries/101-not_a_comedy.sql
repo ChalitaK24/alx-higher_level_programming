edd174f2805ba2ff53d6aca7f8ad6eea7b7bbf56
ChalitaK24 <schalitakel@gmail.com>
2024-11-24 04:33:37 -0800

    sqql18cr

--- 0x0E-SQL_more_queries/101-not_a_comedy.sql
+++ 0x0E-SQL_more_queries/101-not_a_comedy.sql
@@ -1,5 +1,5 @@
 -- lists all shows without the genre Comedy in the database hbtn_0d_tvshows.
-SELECT tv_genres.name
-FROM tv_genres 
-WHERE tv_genres.id NOT IN (SELECT tv_show_genres.genre_id FROM tv_show_genres JOIN tv_shows ON tv_show_genres.show_id = tv_shows.id WHERE tv_shows.title = 'Dexter')
-ORDER BY tv_genres.name ASC;
+SELECT tv_shows.title
+FROM tv_shows
+WHERE tv_shows.id NOT IN (SELECT tv_show_genres.show_id FROM tv_show_genres JOIN tv_shows_genres ON tv_show_genres.genre_id = tv_genres.id WHERE tv_genres.name = 'Comedy')
+ORDER BY tv_shows.title ASC;
