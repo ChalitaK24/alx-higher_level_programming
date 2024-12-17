f9496e4c6a250cbefd7d042b62c9afc9cdaa8661
root <schakitakel@gmail.com>
2024-12-16 17:45:47 -0800

    task 4

---
+++ 0x12-javascript-warm_up/4-concat.js
@@ -0,0 +1,10 @@
+#!/usr/bin/node
+
+// prints two arguments passed to it, in the following format: “ is ”
+
+const args = process.argv.slice(2);
+
+const fstArg = args[0] || "undefined";
+const scdArg = args[1] || "undefined";
+
+console.log('${fstArg} is ${scdArg}');
