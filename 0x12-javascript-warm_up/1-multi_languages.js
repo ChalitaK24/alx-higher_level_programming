baeebb7c7a23bbf0728649043e2509cff52cc178
root <schakitakel@gmail.com>
2024-12-16 17:13:28 -0800

    task

---
+++ 0x12-javascript-warm_up/1-multi_languages.js
@@ -0,0 +1,11 @@
+#!/usr/bin/node
+
+// prints 3 lines
+
+const line1 = "C is fun";
+const line2 = "Python is cool";
+const line3 = "Javascript is amazing";
+
+console.log(line1);
+console.log(line2);
+console.log(line3);
